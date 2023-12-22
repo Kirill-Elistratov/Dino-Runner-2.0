456dec03a7a41491e2ac878f08fee532b34caef3
noskov.is0042 <ivann400@yandex.ru>
2023-12-22 15:35:57 +0300

    Заготовки окон

---
+++ main.py
@@ -0,0 +1,20 @@
+import pygame
+import random
+import os
+import sys
+
+
+class StartWindow(pygame.sprite.Sprite):
+    pass
+
+
+class Menu(pygame.sprite.Sprite):
+    pass
+
+
+class Game(pygame.sprite.Sprite):
+    pass
+
+
+class Pause(pygame.sprite.Sprite):
+    pass
