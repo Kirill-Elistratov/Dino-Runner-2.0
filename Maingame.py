da56e811d64a1a1fddd0c1417f4769405c949bd2
Kirill-Elistratov <elistratovkirill3@gmail.com>
2024-02-28 19:16:12 +0500

    Добавляем начально меню в игре

--- Maingame.py
+++ Maingame.py
@@ -273,5 +273,31 @@ def main():
         background(player.dino_dead)
         cloud.draw(SCREEN)
 
-    clock.tick(60)
-    pygame.display.update()
+        clock.tick(60)
+        pygame.display.update()
+
+
+def menu(death_count):
+    global points
+    run = True
+    while run:
+        SCREEN.fill((255, 255, 255))
+        font = pygame.font.Font('freesansbold.ttf', 30)
+        if death_count == 0:
+            text = font.render("Press any Key to Start", True, (0, 0, 0))
+        elif death_count > 0:
+            score = font.render("Your Score: " + str(points), True, (0, 0, 0))
+            scoreRect = score.get_rect()
+            scoreRect.center = (SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2 + 50)
+            SCREEN.blit(score, scoreRect)
+        textRect = text.get_rect()
+        textRect.center = (SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
+        SCREEN.blit(text, textRect)
+        SCREEN.blit(RUNNING[0], (SCREEN_WIDTH // 2 - 20, SCREEN_HEIGHT // 2 - 140))
+        pygame.display.update()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+            if event.type == pygame.KEYDOWN:
+                main()
