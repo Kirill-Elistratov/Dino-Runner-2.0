78a7561922960f924e1d7af1af159efcd159d1ef
Kirill-Elistratov <elistratovkirill3@gmail.com>
2024-02-28 19:22:52 +0500

    Обновляем файл запуска и удаляем ненужные файлы

--- main.py
+++ main.py
@@ -2,11 +2,10 @@ import pygame
 import random
 import os
 import sys
-
-from game import Game
+from menu import Game
 
 g = Game()
 
 while g.running:
     g.curr_menu.display_menu()
-    g.game_loop()
+    pass
