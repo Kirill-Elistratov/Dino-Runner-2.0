3f0bc7ee29b86a601d9c27c8fa2da85354757b5f
Kirill-Elistratov <elistratovkirill3@gmail.com>
2024-02-28 18:51:31 +0500

    Создаём класс Препятствия и его логику,чтобы в дальнейшем использовать его как наследуемый класс для всех видов пряпятствий

--- Maingame.py
+++ Maingame.py
@@ -138,3 +138,23 @@ class Cloud:
 
     def draw(self, SCREEN):
         SCREEN.blit(self.image, (self.x, self.y))
+
+
+class Obstacle:
+    def __init__(self, image, type):
+        self.image = image
+        self.type = type
+        self.rect = self.image[self.type].get_rect()
+        self.rect.x = SCREEN_WIDTH
+        self.is_stop=False
+
+    def update(self, is_dead):
+        if is_dead:
+            self.is_stop=True
+        if not self.is_stop:
+            self.rect.x -= game_speed
+            if self.rect.x < -self.rect.width:
+                obstacles.pop()
+
+    def draw(self, SCREEN):
+        SCREEN.blit(self.image[self.type], self.rect)
