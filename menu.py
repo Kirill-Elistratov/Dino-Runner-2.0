7f67b52e5f80bef47a356c20805272d0105df394
Kirill-Elistratov <elistratovkirill3@gmail.com>
2024-02-29 15:54:49 +0500

    Изменяем пути файлов

--- menu.py
+++ menu.py
@@ -1,4 +1,10 @@
+import pygame.mixer
 import pygame
+
+from Maingame import menu
+
+click_sound = pygame.mixer.Sound('data/Default_dino/Sounds/click_sound.mp3')
+menu_sound = pygame.mixer.Sound('data/Default_dino/Sounds/menu_sound.mp3')
 
 
 class Menu:
@@ -24,7 +30,7 @@ class MainMenu(Menu):
         self.state = "Start"
         self.startx, self.starty = self.mid_w, self.mid_h + 30
         self.optionsx, self.optionsy = self.mid_w, self.mid_h + 50
-        self.skinsx, self.skinsy = self.mid_w, self.mid_h + 70
+        self.secretx, self.secrety = self.mid_w, self.mid_h + 70
         self.cursor_rect.midtop = (self.startx + self.offset, self.starty)
 
     def display_menu(self):
@@ -33,10 +39,10 @@ class MainMenu(Menu):
             self.game.check_events()
             self.check_input()
             self.game.display.fill(self.game.BLACK)
-            self.game.draw_text('Main Menu', 20, self.game.DISPLAY_W / 2, 20)
-            self.game.draw_text("Start Game", 20, self.startx, self.starty)
-            self.game.draw_text("Options", 20, self.optionsx, self.optionsy)
-            self.game.draw_text("Skins", 20, self.skinsx, self.skinsy)
+            self.game.draw_text('Dino Runner 2', 20, self.game.DISPLAY_W / 2, 20)
+            self.game.draw_text("Start game", 20, self.startx, self.starty)
+            self.game.draw_text("options", 20, self.optionsx, self.optionsy)
+            self.game.draw_text("secret", 20, self.secretx, self.secrety)
             self.draw_cursor()
             self.blit_screen()
 
@@ -46,19 +52,19 @@ class MainMenu(Menu):
                 self.cursor_rect.midtop = (self.optionsx + self.offset, self.optionsy)
                 self.state = 'Options'
             elif self.state == 'Options':
-                self.cursor_rect.midtop = (self.skinsx + self.offset, self.skinsy)
-                self.state = 'Skins'
-            elif self.state == 'Skins':
+                self.cursor_rect.midtop = (self.secretx + self.offset, self.secrety)
+                self.state = 'secret'
+            elif self.state == 'secret':
                 self.cursor_rect.midtop = (self.startx + self.offset, self.starty)
                 self.state = 'Start'
         elif self.game.UP_KEY:
             if self.state == 'Start':
-                self.cursor_rect.midtop = (self.skinsx + self.offset, self.skinsy)
-                self.state = 'Skins'
+                self.cursor_rect.midtop = (self.secretx + self.offset, self.secrety)
+                self.state = 'secret'
             elif self.state == 'Options':
                 self.cursor_rect.midtop = (self.startx + self.offset, self.starty)
                 self.state = 'Start'
-            elif self.state == 'Skins':
+            elif self.state == 'secret':
                 self.cursor_rect.midtop = (self.optionsx + self.offset, self.optionsy)
                 self.state = 'Options'
 
@@ -66,10 +72,11 @@ class MainMenu(Menu):
         self.move_cursor()
         if self.game.START_KEY:
             if self.state == 'Start':
-                self.game.playing = True
+                menu_sound.stop()
+                self.game.game_loop()
             elif self.state == 'Options':
                 self.game.curr_menu = self.game.options
-            elif self.state == 'Skins':
+            elif self.state == 'secret':
                 self.game.curr_menu = self.game.credits
             self.run_display = False
 
@@ -77,10 +84,11 @@ class MainMenu(Menu):
 class OptionsMenu(Menu):
     def __init__(self, game):
         Menu.__init__(self, game)
-        self.state = 'Volume'
-        self.volx, self.voly = self.mid_w, self.mid_h + 20
+        self.state = 'Music'
+        self.music_state = 'on'
+        self.musicx, self.musicy = self.mid_w, self.mid_h + 15
         self.controlsx, self.controlsy = self.mid_w, self.mid_h + 40
-        self.cursor_rect.midtop = (self.volx + self.offset, self.voly)
+        self.cursor_rect.midtop = (self.musicx + self.offset, self.musicy)
 
     def display_menu(self):
         self.run_display = True
@@ -89,7 +97,7 @@ class OptionsMenu(Menu):
             self.check_input()
             self.game.display.fill((0, 0, 0))
             self.game.draw_text('Options', 20, self.game.DISPLAY_W / 2, self.game.DISPLAY_H / 2 - 30)
-            self.game.draw_text("Volume", 15, self.volx, self.voly)
+            self.game.draw_text(f"Music {self.music_state}", 15, self.musicx + self.offset + 100, self.musicy)
             self.game.draw_text("Controls", 15, self.controlsx, self.controlsy)
             self.draw_cursor()
             self.blit_screen()
@@ -99,17 +107,27 @@ class OptionsMenu(Menu):
             self.game.curr_menu = self.game.main_menu
             self.run_display = False
         elif self.game.UP_KEY or self.game.DOWN_KEY:
-            if self.state == 'Volume':
+            if self.state == 'Music':
                 self.state = 'Controls'
                 self.cursor_rect.midtop = (self.controlsx + self.offset, self.controlsy)
             elif self.state == 'Controls':
-                self.state = 'Volume'
-                self.cursor_rect.midtop = (self.volx + self.offset, self.voly)
+                self.state = 'Music'
+                self.cursor_rect.midtop = (self.musicx + self.offset, self.musicy)
         elif self.game.START_KEY:
-            pass
+            if self.state == 'Music':
+                if self.music_state == 'on':
+                    self.music_state = 'off'
+                    menu_sound.stop()
+                else:
+                    self.music_state = 'on'
+                    menu_sound.play(-1)
+            elif self.state == 'Controls':
+                self.game.curr_menu = self.game.controls
+                self.run_display = False
 
 
-class SkinsMenu(Menu):
+
+class ControlsMenu(Menu):
     def __init__(self, game):
         Menu.__init__(self, game)
 
@@ -121,6 +139,78 @@ class SkinsMenu(Menu):
                 self.game.curr_menu = self.game.main_menu
                 self.run_display = False
             self.game.display.fill(self.game.BLACK)
-            self.game.draw_text('Nothing', 20, self.game.DISPLAY_W / 2, self.game.DISPLAY_H / 2 - 20)
-            self.game.draw_text('Will be soon', 15, self.game.DISPLAY_W / 2, self.game.DISPLAY_H / 2 + 10)
+            self.game.draw_text('Press Up to Jump', 20, self.game.DISPLAY_W / 2,
+                                self.game.DISPLAY_H / 2 - 20)
+            self.game.draw_text('Press Backspace to Return', 20, self.game.DISPLAY_W / 2,
+                                self.game.DISPLAY_H / 2 + 10)
             self.blit_screen()
+
+
+class SecretMenu(Menu):
+    def __init__(self, game):
+        Menu.__init__(self, game)
+
+    def display_menu(self):
+        self.run_display = True
+        while self.run_display:
+            self.game.check_events()
+            if self.game.BACK_KEY:
+                self.game.curr_menu = self.game.main_menu
+                self.run_display = False
+            self.game.display.fill(self.game.BLACK)
+            self.game.draw_text('It is a very big secret of this game', 20, self.game.DISPLAY_W / 2,
+                                self.game.DISPLAY_H / 2 - 20)
+            self.game.draw_text('Try to know it', 15, self.game.DISPLAY_W / 2, self.game.DISPLAY_H / 2 + 10)
+            self.blit_screen()
+
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.running, self.playing = True, False
+        self.UP_KEY, self.DOWN_KEY, self.START_KEY, self.BACK_KEY = False, False, False, False
+        self.DISPLAY_W, self.DISPLAY_H = 800, 600
+        self.display = pygame.Surface((self.DISPLAY_W, self.DISPLAY_H))
+        self.window = pygame.display.set_mode((self.DISPLAY_W, self.DISPLAY_H))
+        self.font_name = 'data/Default_dino/Other/8-BIT WONDER.TTF'
+        self.BLACK, self.WHITE = (0, 0, 0), (255, 255, 255)
+        self.main_menu = MainMenu(self)
+        self.options = OptionsMenu(self)
+        self.credits = SecretMenu(self)
+        self.controls = ControlsMenu(self)
+        self.curr_menu = self.main_menu
+        menu_sound.play(-1)
+
+    def game_loop(self):
+        pygame.display.set_mode((1100, 600))
+        if not menu(death_count=0):
+            self.running = False
+            pygame.quit()
+        pygame.display.set_mode((800, 600))
+
+    def check_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.running, self.playing = False, False
+                self.curr_menu.run_display = False
+                pygame.quit()
+            if event.type == pygame.KEYDOWN:
+                click_sound.play()
+                if event.key == pygame.K_RETURN:
+                    self.START_KEY = True
+                if event.key == pygame.K_BACKSPACE:
+                    self.BACK_KEY = True
+                if event.key == pygame.K_DOWN:
+                    self.DOWN_KEY = True
+                if event.key == pygame.K_UP:
+                    self.UP_KEY = True
+
+    def reset_keys(self):
+        self.UP_KEY, self.DOWN_KEY, self.START_KEY, self.BACK_KEY = False, False, False, False
+
+    def draw_text(self, text, size, x, y):
+        font = pygame.font.Font(self.font_name, size)
+        text_surface = font.render(text, True, self.WHITE)
+        text_rect = text_surface.get_rect()
+        text_rect.center = (x, y)
+        self.display.blit(text_surface, text_rect)
